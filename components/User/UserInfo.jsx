9de19b1a1136beb75d96964af1f641451d6644e9
ali navidi <ali_navidi24@yahoo.com>
2022-07-02 18:52:00 +0430

    some bug fixed!!

--- components/User/UserInfo.jsx
+++ components/User/UserInfo.jsx
@@ -1,8 +1,46 @@
-import Image from "next/image"
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { Bars } from "react-loader-spinner";
+import jwt_decode from "jwt-decode";
+import API from "../../requests/API";
 
 const UserInfo = () => {
+  const [loader, SetLoader] = useState(false);
+  const [user, SetUser] = useState([]);
+
+  useEffect(() => {
+    async function getUserInfo() {
+      let userID = jwt_decode(localStorage.getItem("token"))._id;
+      const option = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "x-auth-token": localStorage.getItem("token"),
+        },
+        redirect: "follow",
+      };
+
+      var result = await API(option, `api/user/getUser/${userID}`);
+
+      if (result.status == 200) {
+        SetLoader(false);
+        SetUser(result.data);
+      } else {
+        toast.error(`${result.data.message}`);
+        SetLoader(false);
+      }
+    }
+
+    getUserInfo();
+  }, []);
   return (
     <div className="user-profile flex">
+      {loader && (
+        <div className="flex loader">
+          {" "}
+          <Bars color="#212121" height={30} width={30} />
+        </div>
+      )}
       <div className="user-info">
         <div className="user-image">
           <Image
